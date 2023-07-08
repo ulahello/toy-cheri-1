6b02e7a9d26e86bd2181b364b32c06ba7b987d7f
Ula Shipman <ula.hello@mailbox.org>
2023-07-07 20:22:19 -0700

    feat(vm-driver::main): show *all* the errors, not just the first

--- vm-driver/src/main.rs
+++ vm-driver/src/main.rs
@@ -90,16 +90,20 @@ fn assemble_init(init: &Path) -> anyhow::Result<Vec<Op>> {
     tracing::trace!("assembling init program");
     let parser = Parser2::new(&init_src);
     let mut ops = Vec::new();
+    let mut err_count: usize = 0;
     for try_op in parser {
         match try_op {
             Ok(op) => ops.push(op),
             Err(err) => {
+                err_count += 1;
                 let mut err_out = BufWriter::new(stderr());
                 pretty_print_parse_err(&mut err_out, init, err)?;
                 writeln!(err_out)?;
-                anyhow::bail!("failed to assemble init program");
             }
         }
+    }
+    if err_count > 0 {
+        anyhow::bail!("failed to assemble init program due to {err_count} previous errors");
     }
     Ok(ops)
 }
