ebda3627ee357d513765aecc0112f6a553dc3ea1
Ula Shipman <ula.hello@mailbox.org>
2023-07-30 12:22:04 -0700

    chore(asm::examples): add explicit load z0 to a3 before AllocStat

--- asm/examples/alloc.asm
+++ asm/examples/alloc.asm
@@ -5,7 +5,8 @@ loadi a4, 18446744073709551680 ; 64, 0, 0, 0, 0, 0, 0, 0 | 1, 0, 0, 0, 0, 0, 0
 syscall
 
 ; statting allocator
-loadi  a2, SYS_ALLOC_STAT
+loadi a2, SYS_ALLOC_STAT
+loadc a3, z0 ; not strictly necessary because z0 is already written to a3
 syscall
 
 exit:
