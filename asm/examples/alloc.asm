0e84c13330ba7b02f99e4fbc224119ef7f026a22
Ula Shipman <ula.hello@mailbox.org>
2023-08-05 00:04:37 -0700

    fix(asm::examples): alloc.asm: use new cpy op

--- asm/examples/alloc.asm
+++ asm/examples/alloc.asm
@@ -1,13 +1,13 @@
 ; request allocation from parent allocator
 loadi a2, SYS_ALLOC_ALLOC
-loadc a3, z0
+cpy a3, z0
 ; TODO: support structs as immediate values
 loadi a4, 18446744073709551680 ; Layout { size = 64, align = Align(1) }
 syscall
 
 ; statting allocator
 loadi a2, SYS_ALLOC_STAT
-loadc a3, z0 ; not strictly necessary because z0 is already written to a3
+cpy a3, z0 ; not strictly necessary because z0 is already written to a3
 syscall
 
 exit:
