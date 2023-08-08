f1cf548d9e8a37c6d7d59ef73b41f8d6ba2e9f43
Ula Shipman <ula.hello@mailbox.org>
2023-08-08 11:36:17 -0700

    chore(vm::capability): use shorter invocations

--- vm/src/capability.rs
+++ vm/src/capability.rs
@@ -312,13 +312,13 @@ impl TaggedCapability {
         /* TODO: is it okay if `endb < start`? should we invalidate it now or
          * wait for access to raise exception? */
         // HACK: address should be const comparable
-        let valid = start.get() >= self.capa.start.get() && endb.get() <= self.capa.endb.get();
+        let valid = start.get() >= self.start().get() && endb.get() <= self.endb().get();
         Self {
             capa: Capability {
-                addr: self.capa.addr,
+                addr: self.addr(),
                 start,
                 endb,
-                perms: self.capa.perms,
+                perms: self.perms(),
             },
             valid,
         }
