93248c5134e0f30cfa2fe42e5663a66d5a8dfc50
Ula Shipman <ula.hello@mailbox.org>
2023-08-07 20:40:31 -0700

    fix(vm::alloc): compilation

--- vm/src/alloc.rs
+++ vm/src/alloc.rs
@@ -271,7 +271,7 @@ pub fn free_all(ator: TaggedCapability, mem: &mut Memory) -> Result<(), Exceptio
             bump.free_all();
             revoke::by_bounds(mem, bump.inner.start(), bump.inner.endb())?;
             if header.flags.contains(InitFlags::INIT_ON_FREE) {
-                mem.memset(bump.inner, bump.inner.capability().len(), UNINIT_BYTE)?;
+                mem.memset(bump.inner, bump.inner.capability().span_len(), UNINIT_BYTE)?;
             }
             mem.write(bump_cap, bump)?;
         }
